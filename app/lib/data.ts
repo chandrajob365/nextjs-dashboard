380bf64e6261620774f85f0f1d85dddf7cb7247b
chandrajob365 <chandrajob365@gmail.com>
2026-03-22 17:02:08 +0530

    Data fetching strategy

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -16,12 +16,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 15000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetchRevenue completed after 15 seconds.');
 
     return data;
   } catch (error) {
@@ -43,6 +43,12 @@ export async function fetchLatestInvoices() {
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
+
+    console.log('Fetching invoice data...');
+    await new Promise((resolve) => setTimeout(resolve, 10000));
+
+    console.log('Data fetchLatestInvoices completed after 10 seconds.');
+    
     return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
@@ -72,6 +78,11 @@ export async function fetchCardData() {
     const numberOfCustomers = Number(data[1][0].count ?? '0');
     const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
     const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
+
+    console.log('Fetching card data...');
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    console.log('Data fetchCardData completed after 5 seconds.');
 
     return {
       numberOfCustomers,
